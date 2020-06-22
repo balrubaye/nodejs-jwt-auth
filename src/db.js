582f71c0ea756516b2914e641e1c6e3ce5a53f59
Bassam Rubaye <bassam.rubaye@gmail.com>
2020-06-22 14:11:02 -0700

    Create auth  db and service layers

---
+++ src/db.js
@@ -0,0 +1,8 @@
+const Datastore = require("nedb");
+const { promisify } = require("util");
+
+const db = new Datastore();
+db.insertPromise = promisify(db.insert);
+db.findOnePromise = promisify(db.findOne);
+
+module.exports = db;
